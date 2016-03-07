9a4e3d7303d859d96cb75e7c8c08c961de97b815
Zaktansnake <david_chongym@outlook.com>
2016-03-08 00:46:40 +0800

    Wrote basic lines for code.

--- src/A0116633.java
+++ src/A0116633.java
@@ -1,4 +1,15 @@
+import java.io.*;
+import java.util.*;
 
 public class A0116633 {
 
+	private static String fileName;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		fileName = sc.next();
+		
+		File file = new File(fileName);
+		
+	}
 }
