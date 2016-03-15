ce5531ef13fd318ee2b9a4aed38d1f3000ede0e0
David Chong <zaktansnake@outlook.com>
2016-03-16 04:29:08 +0800

    Updated code

--- src/A0116633.java
+++ src/A0116633.java
@@ -3,24 +3,27 @@ import java.util.*;
 
 public class A0116633 {
 	
+	private static double[][] inputArray;
 	private static double[] iArray, jArray;
-	private static int globalCounter;
-	private static double minimalI, minimalJ, value;
+	private static int globalCounterI, globalCounterJ, minimalI, minimalJ;
+	private static double value;
 	
 	public static void main(String[] args) {
+		inputArray = new double[25][25];
 		iArray = new double[25];
 		jArray = new double[25];
-		globalCounter = 0;
+		globalCounterI = 0;
+		globalCounterJ = 0;
 		
 		Scanner sc = new Scanner(System.in);
 		String fileName = sc.next();
 		
-		readFile(fileName);
+		findCenter(fileName);
 		
 		sc.close();
 	}
 	
-	public static void readFile(String fileName) {
+	public static void findCenter(String fileName) {
 		FileReader fr = null;
 		BufferedReader br = null;
 		PrintWriter pw = null;
@@ -35,9 +38,15 @@ public class A0116633 {
 				fileInput = br.readLine();
 				calculateISum(fileInput);
 				calculateJSum(fileInput);
-				globalCounter++;
+				globalCounterI++;
 			}
 			
+			for(int i = 0 ; i <= 25; i++) {
+				minimalI = findMinimalI();
+				minimalJ = findMinimalJ();
+			}
+			
+			value = inputArray[minimalI][minimalJ];
 			String outputFileName = fileName + ".txt";
 			File outputFile = new File(outputFileName);
 			pw = new PrintWriter(outputFile);
@@ -62,26 +71,49 @@ public class A0116633 {
 			for(int i = 0; i <= fileInput.length(); i = i + 4) {
 				String extractedNumber = fileInput.substring(i, i+3);
 				double number = Double.parseDouble(extractedNumber);
+				inputArray[globalCounterI][globalCounterJ] = number;
+				globalCounterJ++;
 				totalSum = totalSum + number;
 			}
 			
-			iArray[globalCounter] = totalSum;
+			globalCounterJ = 0;
+			iArray[globalCounterI] = totalSum;
 		} catch (Exception e) {
-			System.err.println("Error when reeading file!");
+			System.err.println("Error when reading file!");
 		}
 	}
 	
 	public static void calculateJSum(String fileInput) {
-		double totalSum = 0.00;
-		
 		try {
 			for(int i = 0; i <= fileInput.length(); i = i + 4) {
 				String extractedNumber = fileInput.substring(i, i+3);
-				double number = Double.parseDouble(extractedNumber);	
+				double number = Double.parseDouble(extractedNumber);
+				jArray[globalCounterJ] = jArray[globalCounterJ] + number;
+				globalCounterJ++;
 			}
+			globalCounterJ = 0;
 		} catch (Exception e) {
-			System.err.println("Error when reeading file!");
+			System.err.println("Error when reading file!");
 		}
 	}
 	
+	public static int findMinimalI() {
+		int minimal = 0;
+		
+		for(int i = 0; i <= 23; i++) {
+			
+		}
+		
+		return minimal;
+	}
+	
+	public static int findMinimalJ() {
+		int minimal = 0;
+		
+		for(int i = 0; i <= 23; i++) {
+			
+		}
+		
+		return minimal;
+	}
 }
